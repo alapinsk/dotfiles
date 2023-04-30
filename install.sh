ff04e269acdd5a1fb0b3261f0e5ae0eb4a9416e1
Andrzej Łapiński <a.lapinski@outlook.com>
2023-04-30 20:33:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,11 +40,11 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
 echo "Create symlinks!" 
 create_symlinks
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 
 sudo apt install bat
 mkdir -p ~/.local/bin
