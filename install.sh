8c181d1c2d0e1a1c86b68b65060817b9fe145322
Andrzej Łapiński <a.lapinski@outlook.com>
2023-05-02 07:30:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,12 @@ conda init zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-sh -c "$(curl -fsSL https://starship.rs/install.sh)" -y -f
-
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 yes | sh -c ~/.fzf/install -y -f
 
 echo "generate symlinks"
 ln -s -f ~/dotfiles/.zshrc  ~/.zshrc
-ln -s -f ~/dotfiles/.config/starship.toml  ~/.config/starship.toml
